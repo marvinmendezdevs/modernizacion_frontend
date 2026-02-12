422714f24634ec62f778f0b84bcd5e7e98fa189d
Alessandro Martinez <alessandrojosue53@gmail.com>
2026-02-12 15:04:24 -0600

    Mostrando los enlaces del tutor virtual

--- src/components/pages/TutorshipInfoTutores.tsx
+++ src/components/pages/TutorshipInfoTutores.tsx
@@ -1,5 +1,6 @@
 import type { TutorCountType } from "@/types/auth.types";
-import type { Dispatch, SetStateAction } from "react";
+import { Funnel } from "lucide-react";
+import { useMemo, useState, type Dispatch, type SetStateAction } from "react";
 import { Link } from "react-router";
 
 type TutorshipInfoTutoresProps = {
@@ -13,10 +14,43 @@ type TutorshipInfoTutoresProps = {
 }
 
 function TutorshipInfoTutores({ tutor, meta, setPage }: TutorshipInfoTutoresProps) {
+        const getTodayDate = () => new Date().toLocaleDateString('sv-SE');
+        const [today, setToday] = useState(getTodayDate());
+
+        const data = useMemo(() => {
+          return tutor.map((t) => {
+            const sesionesHoy = t.virtualSesions.filter(
+              (s) => s.date.slice(0, 10) === today
+            );
+
+            return {
+              ...t,
+
+              enlacesVirtual: {
+                recording: sesionesHoy.filter((s) => s.recording?.trim() !== "").length,
+                transcription: sesionesHoy.filter((s) => s.transcription?.trim() !== "").length,
+                attendance: sesionesHoy.filter((s) => s.attendance?.trim() !== "").length,
+                quizz: sesionesHoy.filter((s) => s.quizz?.trim() !== "").length,
+              }
+            };
+          });
+        }, [tutor, today]);
+
+
+
+        console.log(data);
 
   return (
     <>
-      <p className="text-indigo-600 font-bold my-5">Tutores {String(tutor[0].infoTutores.type) === "PRESENCIAL" ? "Presenciales" : "Virtuales"}</p>
+      <div className="flex justify-between items-center">
+        <p className="text-indigo-600 font-bold my-5">Tutores {String(tutor[0].infoTutores.type) === "PRESENCIAL" ? "Presenciales" : "Virtuales"}</p>
+        <div className="flex items-center gap-2">
+          <Funnel className="size-5 text-gray-700"/>
+          <p className="text-gray-600">Filtrar</p>
+          <input className="p-2 bg-white border border-gray-200 rounded-lg" type="date" value={today} onChange={ e => setToday(e.target.value) } />
+        </div>
+      </div>
+
       <div className="overflow-x-auto ">
         <table className="w-full table-auto">
           <thead
@@ -24,35 +58,51 @@ function TutorshipInfoTutores({ tutor, meta, setPage }: TutorshipInfoTutoresProp
             <tr>
               <th className="w-76 px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider rounded-tl-lg">TUTOR</th>
               <th className="px-4 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider text-center">TELÉFONO</th>
-              {String(tutor[0].infoTutores.type) !== "VIRTUAL" && (
+              {String(tutor[0].infoTutores.type) === "VIRTUAL" ? (
+                <th className="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">GRABACIÓN</th>
+              ):(
                 <th className="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">DISTRITO</th>
               )}
-              {String(tutor[0].infoTutores.type) !== "VIRTUAL" && (
+              {String(tutor[0].infoTutores.type) === "VIRTUAL" ? (
+                <th className="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">TRANSCRIPCIÓN</th>
+              ):(
                 <th className="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">DIAGNÓSTICO</th>
               )}
-              {String(tutor[0].infoTutores.type) !== "VIRTUAL" && (
+              {String(tutor[0].infoTutores.type) === "VIRTUAL" ? (
+                <th className="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">ASISTENCIA</th>
+              ):(
                 <th className="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">OBSERVACIONES</th>
               )}
-              {String(tutor[0].infoTutores.type) !== "VIRTUAL" && (
+              {String(tutor[0].infoTutores.type) === "VIRTUAL" ? (
+                <th className="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">QUIZZ</th>
+              ):(
                 <th className="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider rounded-tr-lg">RETROALIMENTACIONES</th>
               )}
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-200">
-            {tutor.map((tutor, index) => (
+            {data.map((tutor, index) => (
               <tr key={index}>
                 <td className="w-76 px-4 py-4 whitespace-nowrap flex flex-col"><Link to={`/tutoria/tutor/${tutor.username}`} className="flex flex-col"><span className="text-xs font-medium text-gray-900">{tutor.name}</span><span className="text-xs text-gray-500">{tutor.email}</span></Link></td>
                 <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500 text-center">{tutor.telephone}</td>
-                {String(tutor.infoTutores.type) !== "VIRTUAL" && (
+                {String(tutor.infoTutores.type) === "VIRTUAL" ? (
+                  <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500 text-center">{tutor.enlacesVirtual.recording}</td>
+                ):(
                   <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500 text-center">{tutor.infoTutores.districts?.district ?? "-"}</td>
                 )}
-                {String(tutor.infoTutores.type) !== "VIRTUAL" && (
+                {String(tutor.infoTutores.type) === "VIRTUAL" ? (
+                  <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500 text-center">{tutor.enlacesVirtual.transcription}</td>
+                ):(
                   <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500 text-center">{tutor.countDiagnostico}</td>
                 )}
-                {String(tutor.infoTutores.type) !== "VIRTUAL" && (
+                {String(tutor.infoTutores.type) === "VIRTUAL" ? (
+                  <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500 text-center">{tutor.enlacesVirtual.attendance}</td>
+                ):(
                   <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500 text-center">{tutor.countObservaciones}</td>
                 )}
-                {String(tutor.infoTutores.type) !== "VIRTUAL" && (
+                {String(tutor.infoTutores.type) === "VIRTUAL" ? (
+                  <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500 text-center">{tutor.enlacesVirtual.quizz}</td>
+                ):(
                   <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500 text-center">{tutor.countRetroalimentaciones}</td>
                 )}
               </tr>
--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,8 +1,10 @@
 import type z from "zod";
 import type { InfoTutorCountResponseSchema, LoginResponseSchema, LoginSchema, UserSchema } from "@/schemas/auth.schema";
+import type { VirtualSessionShema } from "@/schemas/tutorship.schema";
 
 export type UserType = z.infer<typeof UserSchema>
 export type LoginType = z.infer<typeof LoginSchema>
 export type LoginResponseType = z.infer<typeof LoginResponseSchema>
 
 export type TutorCountType = z.infer<typeof InfoTutorCountResponseSchema>
+export type VirtualSessionType = z.infer<typeof VirtualSessionShema>;
